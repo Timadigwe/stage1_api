ccc458fcbd5227606fcc3bf636d09a3d66c9a956
timadigwe <timothyadigwe2019@gmail.com>
2023-09-11 08:51:28 +0100

    api created

---
+++ src/api/api.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import axios from 'axios';
+import * as moment from 'moment';
+
+@Controller('api')
+export class ApiController {
+  @Get()
+  async getInfo(
+    @Query('slack_name') slackName: string,
+    @Query('track') track: string,
+  ) {
+    const currentDay = moment().format('dddd');
+    const utcTime = moment.utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
+    const githubFileUrl =
+      'https://github.com/username/repo/blob/main/file_name.ext';
+    const githubRepoUrl = 'https://github.com/username/repo';
+
+    const response = {
+      slack_name: slackName,
+      current_day: currentDay,
+      utc_time: utcTime,
+      track: track,
+      github_file_url: githubFileUrl,
+      github_repo_url: githubRepoUrl,
+      status_code: 200,
+    };
+
+    return response;
+  }
+}
